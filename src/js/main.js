71049a87de8410a1c1761a10b9e1bf0973572dba
Jennifer Jones <jybjones@gmail.com>
2015-06-03 11:29:02 -0500

    reverted back to AngularAddress forms and then added bootstrap

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,27 @@
-var angularForms = angular.module("angularForms", []);
+angular
+  .module('angularAddresses', [])
 
-angularForms.controller('AddressBookController', function () {
-  var vm = this;
+  .controller('Main', function () {
+    var vm = this;
 
-  vm.people = [
-      {firstName:"Abe", lastName: "Abel", number: "111-111-1111", twitter: "@abe"},
-      {firstName:"Busy", lastName: "Body", number: "222-222-2222", twitter: "@busy"},
-      {firstName:"Candy", lastName: "Crush", number: "333-333-3333", twitter: "@candy"}
-  ];
+    vm.people = [
+      {name: 'Ben', twitter: '@ben123', phone: '+1 615 234 5678'},
+      {name: 'Dan', twitter: '@dandan', phone: '(615) 234-5678'},
+      {name: 'Elsa', twitter: '@letitgo', phone: '615-234-5678'},
+      {name: 'Amanda', twitter: '@princessamanda', phone: '1 615 234 5678'},
+      {name: 'Charity', twitter: '@nonprofit', phone: '615.234.5678'}
+    ];
 
-  vm.newPerson = {};
-
-  vm.addNewAddress = function () {
-    vm.people.push(vm.newPerson);
     vm.newPerson = {};
-  };
 
-  vm.removeAddress = function (person) {
-    var index = vm.people.indexOf(person);
-    vm.people.splice(index, 1);
-  };
+    vm.addNewAddress = function () {
+      vm.people.push(vm.newPerson);
+      vm.newPerson = {};
+    };
 
-});
+    vm.removeAddress = function (person) {
+      var index = vm.people.indexOf(person);
+      vm.people.splice(index, 1);
+    };
 
-// $scope.addContact = function() {
-//     if($scope.firstName && $scope.lastName && $scope.number && $scope.twitter)
-//     {
-//       $scope.contacts.push({firstName:$scope.firstName, lastName:$scope.lastName, number:$scope.number, twitter:$scope.twitter})
-//       $scope.firstName ='';
-//       $scope.lastName ='';
-//       $scope.number = '';
-//       $scope.twitter ='';
-//     }
-// };
-
-
-
+  });
