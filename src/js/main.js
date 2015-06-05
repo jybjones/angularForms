00b340c1ce78d261eb87877d5c0aafd71dc9ae3e
Jennifer Jones <jybjones@gmail.com>
2015-06-05 12:00:05 -0500

    created routes and partials

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,26 @@
 angular
-  .module('angularAddresses', [])
+  .module('angularAddresses', ['ngRoute'])
+
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/landing.html'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html'
+      })
+      .when('/people', {
+        templateUrl: 'views/people.html',
+        controller: 'Main',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        templateUrl: 'views/404.html'
+      });
+  })
 
   .filter('objToArr', function () {
     return function (obj) {
@@ -7,7 +28,7 @@ angular
         return Object
         .keys(obj)
         .map(function (key) {
-          obj[key]['_id'] = key;
+          obj[key]['_id'] = key; //this is the entire token//
         return obj[key];
           });
       }
@@ -39,19 +60,21 @@ angular
     vm.addNewAddress = function () {
       $http
         .post('https://viewangularapp.firebaseio.com/people.json', vm.newPerson)
-        .success(function () {
-          vm.people.push(vm.newPerson);
+        .success(function (res) {
+          vm.people[res.name] = vm.newPerson;
           vm.newPerson = {};
           $('#modal').modal('hide');
         });
     };
 
-    vm.removeAddress = function (id) {
+    vm.removeAddress = function (key) {
+      console.log(key, "yo")
       $http
-        .delete(`https://viewangularapp.firebaseio.com/people/${id}.json`)
+        .delete(`https://viewangularapp.firebaseio.com/people/${key}.json`)
         .success(function () {
-          delete vm.people[id]
-        });
+          delete vm.people[key];
+        })
+
     };
 
   });
